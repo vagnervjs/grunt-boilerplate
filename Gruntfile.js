0e7ae02a91302876f6cb5e731beebcd33eaa8c46
vagnervjs <vagnervjs@gmail.com>
2013-03-20 12:06:02 -0300

    Lint Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,49 @@
 module.exports = function(grunt) {
-  var gruntConfig = {
-    pkg: grunt.file.readJSON('package.json'),
-    min: {
-      dist: {
-        src: ['src/assets/js/main.js'],
-        dest: 'src/assets/js/all.min.js'
-      }
-    },
-    cssmin: {
-      dist: {
-        src: ['src/assets/css/main.css'],
-        dest: 'src/assets/css/all.min.css'
-      }
-    },
-    rsync: {
-      dist: {
-        src: './src/',
-        dest: './dist',
-        recursive: true,
-        syncDest: true,
-        exclude: ['main.*']
-      },
-      deploylive: {
-        src: './dist/',
-        dest: '/var/www/grunt',
-        host: 'root@vagnersantana.com',
-        recursive: true,
-        syncDest: true
-      }
-    }
-  };
-  grunt.initConfig(gruntConfig);
-  var keys = Object.keys(gruntConfig);
-  var tasks = [];
-  //Loop trough keys, skipping pkg
-  for(i = 1, len = keys.length; i < len; i++) {
-    tasks.push(keys[i]);
-  }
-  grunt.loadNpmTasks('grunt-yui-compressor');
-  grunt.loadNpmTasks('grunt-rsync');
-  grunt.registerTask('default', tasks);
+	'use strict';
+	var gruntConfig = {
+		pkg: grunt.file.readJSON('package.json'),
+		min: {
+			dist: {
+				src: ['src/assets/js/main.js'],
+				dest: 'src/assets/js/all.min.js'
+			}
+		},
+		cssmin: {
+			dist: {
+				src: ['src/assets/css/main.css'],
+				dest: 'src/assets/css/all.min.css'
+			}
+		},
+		rsync: {
+			dist: {
+				src: './src/',
+				dest: './dist',
+				recursive: true,
+				syncDest: true,
+				exclude: ['main.*']
+			},
+			deploy: {
+				src: './dist/',
+				dest: '/var/www/grunt',
+				host: 'root@vagnersantana.com',
+				recursive: true,
+				syncDest: true
+			}
+		}
+	};
+	grunt.initConfig(gruntConfig);
+
+	var keys = Object.keys(gruntConfig);
+	var tasks = [];
+
+	for(var i = 1, l = keys.length; i < l; i++) {
+		tasks.push(keys[i]);
+		if(i === l-1) {
+			console.log('CHEGOU !!!');
+		}
+	}
+
+	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-rsync');
+	grunt.registerTask('default', tasks);
 };
