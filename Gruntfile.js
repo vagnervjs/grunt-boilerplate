58da6e11c7f7f5a86e76ced98c5c64220327772a
vagnervjs <vagnervjs@gmail.com>
2014-05-23 13:30:22 -0300

    Add imageoptim task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,21 @@ module.exports = function(grunt) {
                 dest: 'src/assets/css/all.min.css'
             }
         },
+        imageoptim: {
+            dist: {
+                options: {
+                    jpegMini: false,
+                    imageAlpha: true,
+                    quitAfter: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/assets/img',
+                    src: '**/*.{png,jpg,jpeg}',
+                    dest: 'src/assets/img-opt'
+                }]
+            }
+        },
         // uncomment for use less and comment cssmin configs
 		// less: {
         //     development: {
@@ -26,23 +41,23 @@ module.exports = function(grunt) {
         //         }
         //     }
         // },
-		rsync: {
-			dist: {
-				src: './src/',
-				dest: './dist',
-				recursive: true,
-				syncDest: true,
-				exclude: ['main.*', 'less']
-			},
-            // uncomment and config
-			// deploy: {
-			// 	src: './dist/',
-			// 	dest: '/var/www',
-			// 	host: 'root@vagnersantana.com',
-			// 	recursive: true,
-			// 	syncDest: true
-			// }
-		}
+		// rsync: {
+		// 	dist: {
+		// 		src: './src/',
+		// 		dest: './dist',
+		// 		recursive: true,
+		// 		syncDest: true,
+		// 		exclude: ['main.*', 'less']
+		// 	},
+  //           // uncomment and config
+		// 	// deploy: {
+		// 	// 	src: './dist/',
+		// 	// 	dest: '/var/www',
+		// 	// 	host: 'root@vagnersantana.com',
+		// 	// 	recursive: true,
+		// 	// 	syncDest: true
+		// 	// }
+		// }
 	};
 	grunt.initConfig(gruntConfig);
 
@@ -54,6 +69,7 @@ module.exports = function(grunt) {
 	}
 
 	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-imageoptim');
 	grunt.loadNpmTasks('grunt-rsync');
     // uncomment for use less
 	// grunt.loadNpmTasks('grunt-contrib-less');
